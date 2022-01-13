42f2331ec7e423846098f87f9bee43193e8ed25d
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-13 17:37:47 -0300

    file uploader component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FileUploaderComponent } from './components/file-uploader/file-uploader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploaderComponent
   ],
   imports: [
     BrowserModule
